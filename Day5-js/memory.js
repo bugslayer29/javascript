0a2efa03e0ae7d232d999462b81e7b9601dc16e8
bugslayer29 <rohitprem2003kr@gmail.com>
2024-12-17 22:03:29 +0530

    new file added

--- Day5-js/memory.js
+++ Day5-js/memory.js
@@ -1,3 +1,36 @@
 // Two types of memory
 // stact memory and heap memory.
+  
+
+// stack memory used in primitive types of data. You get copy of whatever variable you declare. 
+
+
+// Heap memory used in Non-primitive types of data. . You get refrence of original value.
+
+//example of stack memory
+let myYoutubename = "rohitcoding"
+let anotherYoutubename = myYoutubename
+
+anotherYoutubename = "chai or code"
+
+console.log(myYoutubename); //rohitcoding (which is old value)
+console.log(anotherYoutubename); //chai or code
+
+
+
+
+//example of heap memory
+
+let userOne = {
+    email : "user@gmail.com",
+    gpay: "user@eolaxis",
+}
+
+let userTwo = userOne;
+ 
+userTwo.email = "hitesh"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
 
---
+++ Day6-js/string.js
@@ -0,0 +1,14 @@
+const name = "hitesh"
+const repoCount = 50
+
+console.log(name + repoCount + " value"
+);
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`)
+//Backdesk which gives string interpolation like above. It's syntax are like
+// console.log(`Hello I am ${name}`);
+  
+
+
+
+
